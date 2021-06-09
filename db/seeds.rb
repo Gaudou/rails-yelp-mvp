92c17d5a7c9e46aff367cf4d8253562708c3c33d
Gauderic Diagambana <diagambana.gauderic@gmail.com>
2021-06-09 14:09:52 +0200

    Seeds Created - Works fine

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+sogood = Restaurant.new(name: 'So Good', category: 'french', address: '165 avenue de Bretagne, Lille')
+pointg = Restaurant.new(name: 'Le Point G', category: 'belgian', address: 'Mouscron, Belgique')
+tatachese = Restaurant.new(name: 'Tata Cheese', category: 'chinese', address: 'Rue de Paris, Paris')
+nu = Restaurant.new(name: 'Nu Bar & Restaurant', category: 'french', address: 'Rue de la gare, Lille')
+bellazza = Restaurant.new(name: 'La Bellazza', category: 'italian', address: 'Rue de gand, Lille')
+
+sogood.save
+pointg.save
+tatachese.save
+nu.save
+bellazza.save
